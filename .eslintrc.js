7926f8ab2c24a67eec42b31a75765d10516ec63e
소프트원-김동민 <emthrology@gmail.com>
2022-01-07 15:56:10 +0900

    1.applying lint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line prettier/prettier
+// eslint-disable-next-line prettier/prettier
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true, //for test
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    //.prettierrc 대신 eslintrc.js 안에 prettier 룰을 적는다: 충돌방지를 위해
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: false,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: "all",
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: "avoid",
+        endOfLine: "auto",
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
