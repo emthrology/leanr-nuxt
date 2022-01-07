0f4cacf0bdf0a789d1fb728aeedce18025224643
소프트원-김동민 <emthrology@gmail.com>
2022-01-07 16:02:25 +0900

    3.error fallback page 설정

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- layouts에 error.vue 로 생성하면 자동 fallback 연결됨 -->
+    <h1>Error Occured</h1>
+    <p>Page Not Found</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
