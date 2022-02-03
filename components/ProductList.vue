3fe598ff04f9d320b2a87000c9ffe46a23678a80
소프트원-김동민 <emthrology@gmail.com>
2022-02-03 13:53:15 +0900

    nuxt data feature - asyncData()

---
+++ components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="product of products" :key="product.id">
+        {{ product }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    products: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style></style>
--- pages/main.vue
+++ pages/main.vue
@@ -1,30 +1,39 @@
 <template>
   <div>
     <h1>main</h1>
-    <div>
-      <ul>
-        <li v-for="product of products" :key="product.id">
-          {{ product }}
-        </li>
-      </ul>
-    </div>
+    <ProductList :products="products" />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ProductList from '@/components/ProductList.vue';
 export default {
-  //async created 의 문제점 : 데이터 불러오는 동안 빈 상태로 화면을 놔둠
-  async created() {
+  components: {
+    ProductList,
+  },
+  //nuxt data feature - asyncData()
+  //1.async 기본 사용
+  //2.this - vm 바인딩 이전에 작동
+  //3.리턴값이 곧바로 data()처럼 작동
+  //4.pages폴더 안에서만 적용되는 프로퍼티이다.(주의!)
+  async asyncData() {
     const res = await axios.get('http://localhost:3000/products');
     console.log(res);
-    this.products = res.data;
+    const products = res.data;
+    return { products };
   },
-  data() {
-    return {
-      products: [],
-    };
-  },
+  //async created 의 문제점 : 데이터 불러오는 동안 빈 상태로 화면을 놔둠
+  // async created() {
+  //   const res = await axios.get('http://localhost:3000/products');
+  //   console.log(res);
+  //   this.products = res.data;
+  // },
+  // data() {
+  //   return {
+  //     products: [],
+  //   };
+  // },
 };
 </script>
 
