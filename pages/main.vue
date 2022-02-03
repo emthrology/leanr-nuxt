312802b82de917c744a4865f5c352ead6a4a0274
소프트원-김동민 <emthrology@gmail.com>
2022-02-03 12:28:24 +0900

    4.axios 사용하기 (axios response spec 참고)

--- pages/main.vue
+++ pages/main.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>main</div>
+  <div>
+    <h1>main</h1>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+export default {
+  async created() {
+    const res = await axios.get('http://localhost:3000/products');
+    console.log(res);
+  },
+};
 </script>
 
 <style></style>
