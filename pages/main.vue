18c96b97f31da3251606fc168695e3f9f87564cb
소프트원-김동민 <emthrology@gmail.com>
2022-02-03 13:20:50 +0900

    기존 spa lifecycle의 문제점

--- pages/main.vue
+++ pages/main.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
     <h1>main</h1>
+    <div>
+      <ul>
+        <li v-for="product of products" :key="product.id">
+          {{ product }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 export default {
+  //async created 의 문제점 : 데이터 불러오는 동안 빈 상태로 화면을 놔둠
   async created() {
     const res = await axios.get('http://localhost:3000/products');
     console.log(res);
+    this.products = res.data;
+  },
+  data() {
+    return {
+      products: [],
+    };
   },
 };
 </script>
